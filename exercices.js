9bacb3548259d3fd4427240ec53429415700e8ee
Lucas <lucasjaskowiak@yahoo.fr>
2025-01-22 09:02:37 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -2,3 +2,9 @@ const nom_boutique = "Les potions du maitre";
 let nombre_potions_stock = 40;
 let prix_potion = 2;
 let boutique_ouverte = true;
+
+if (!boutique_ouverte) {
+  console.log(`La boutique ${nom_boutique} est fermée, revenez plus tard Aventurier ! 😴`);
+} else {
+  console.log(`Bienvenue dans la boutique ${nom_boutique} Aventurier ! 🎉`);
+}
