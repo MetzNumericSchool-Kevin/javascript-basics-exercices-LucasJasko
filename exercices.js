f953a073f0cd754e83309c0eb1bafffff56e6e68
Lucas <lucasjaskowiak@yahoo.fr>
2025-01-22 09:02:57 +0100

    Affichage conditionnel avec switch

--- exercices.js
+++ exercices.js
@@ -8,3 +8,15 @@ if (!boutique_ouverte) {
 } else {
   console.log(`Bienvenue dans la boutique ${nom_boutique} Aventurier ! 🎉`);
 }
+
+let choix = prompt(`Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔
+1. Le nom de la boutique
+2. Le nom du Sorcier
+3. Le prix d'un potion de soin
+4. La quantité d'une potion de soin`);
+switch (choix) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+}
