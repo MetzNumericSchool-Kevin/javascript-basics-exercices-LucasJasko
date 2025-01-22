4b8b0dac964804a6d500fb12412a4969348a4680
Lucas <lucasjaskowiak@yahoo.fr>
2025-01-22 09:13:26 +0100

    Calcul du prix total d'une commande de potion

--- exercices.js
+++ exercices.js
@@ -1,15 +1,18 @@
+// Définition de variables
 const nom_sorcier = prompt("Comment te nommes-tu, sorcier ? 🧙‍♂️");
 const nom_boutique = "Les potions du maitre";
 let nombre_potions_stock = 40;
 let prix_potion = 2;
 let boutique_ouverte = true;
 
+// Affichage conditionnel
 if (!boutique_ouverte) {
   console.log(`La boutique ${nom_boutique} est fermée, revenez plus tard Aventurier ! 😴`);
 } else {
   console.log(`Bienvenue dans la boutique ${nom_boutique} Aventurier ! 🎉`);
 }
 
+// Affichage conditionnel avec switch
 let choix = prompt(`Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔
 1. Le nom de la boutique
 2. Le nom du Sorcier
@@ -31,3 +34,7 @@ switch (choix) {
   default:
     console.log(`Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix ! 😕`);
 }
+
+// Calcul du prix total d'une commande de potion 🪙
+let qté = prompt(`Combien de potion(s) désires-tu?`);
+console.log(`Prix de ${qté} potions de soins : ${prix_potion * qté} 🪙 mon cher Aventurier. 💸`);
